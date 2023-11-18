8a04aa70ce75eabc39581bda5493ea16d239eaf8
InfinityRem <zagrekovdenis@gmail.com>
2023-11-18 21:20:27 +0300

    )

---
+++ skript.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function calculate() {
+    var result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  }
+
+  function calculateSquareRoot() {
+    var value = document.getElementById('display').value;
+    if (value !== '') {
+      document.getElementById('display').value = Math.sqrt(parseFloat(value));
+    }
+  }
